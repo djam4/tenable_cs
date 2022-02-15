ed92ce9dbd28d5d7fb57720f8a65a7788e5e5a1c
Deepu James <deeps.data@gmail.com>
2022-02-15 20:27:52 +0800

    Added 2 S3

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -1,6 +1,6 @@
-resource "aws_s3_bucket" "tenable_cs_demo_s3_bucket" {
-  bucket = "tenables3bucket"
+resource "aws_s3_bucket" "tenable_cs_demo_s3_bucket2" {
+  bucket = "tenables3bucket2"
   acl    = "public-read"
   tags   = var.default_tags
 }
-# Modified S3 Bucket Name
+# Modified S3 Bucket Name2
