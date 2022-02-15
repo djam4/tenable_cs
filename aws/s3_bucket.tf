d6298257c94195011e23c23cae77daaa0511110f
Deepu James <deeps.data@gmail.com>
2022-02-15 16:08:19 +0800

    DJ Commit

--- aws/s3_bucket.tf
+++ aws/s3_bucket.tf
@@ -1,5 +1,6 @@
 resource "aws_s3_bucket" "tenable_cs_demo_s3_bucket" {
-  bucket = "tenablecss3bucket"
+  bucket = "tenables3bucket"
   acl    = "public-read"
   tags   = var.default_tags
 }
+# Modified S3 Bucket Name
