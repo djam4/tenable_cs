29951d228902e70a58374f63f426879d7b3b6e84
Deepu James <deeps.data@gmail.com>
2022-02-15 21:59:18 +0800

    updated state space

--- provider.tf
+++ provider.tf
@@ -5,8 +5,15 @@ terraform {
       version = "~> 3.47"
     }
   }
-}
 
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "dj-test"
+    workspaces {
+      name = "StateSpace"
+}
+  }
+}
 provider "aws" {
   region = "us-east-2"
 }
