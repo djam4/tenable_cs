b204537e3d0ffdee807c249c712c728f0d42dc2e
Deepu James <deeps.data@gmail.com>
2022-02-17 22:27:51 +0800

    added ec2

---
+++ ec2.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "app_server" {
+  ami           = "ami-074251216af698218"
+  instance_type = "t2.micro"
+
+  user_data = <<-EOF
+  #!/bin/bash -xe
+curl -H 'X-Key: c3572dec9a7a3d3add0630aa54298a1b56a979471c59ae5b741967fa95d1aaf3' 'https://cloud.tenable.com/install/agent?name=Ubuntutest&groups=Group A' | bash
+EOF
+  tags = {
+    Name = "Ubuntu-Terraform"
+  }
+}
