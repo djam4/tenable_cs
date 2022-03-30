713658b48b68c79f6b49e4a1c407b5010f638d18
Accurics <admin@accurics.com>
2022-03-30 14:25:36 +0800

    Auto Remediation By Accurics

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -3,3 +3,28 @@ resource "aws_s3_bucket" "demo-s3-bucket224420" {
   acl    = "private"
 }
 #Added a comment
+
+resource "aws_s3_bucket_policy" "demo-s3-bucket224420Policy" {
+  bucket = "${aws_s3_bucket.demo-s3-bucket224420.id}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "demo-s3-bucket224420-restrict-access-to-users-or-roles",
+      "Effect": "Allow",
+      "Principal": [
+        {
+          "AWS": [
+            <aws_policy_role_arn>
+          ]
+        }
+      ],
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::${aws_s3_bucket.demo-s3-bucket224420.id}/*"
+    }
+  ]
+}
+POLICY
+}
